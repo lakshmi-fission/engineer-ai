d6491298e9e51a39e159abf3a30037907d53ce64
lakshmi.sala <lakshmi.sala@fissionlabs.in>
2019-06-10 17:32:16 +0530

    Implemented Pagination and set title functionality

--- app/src/main/java/com/example/testprogram/MainActivity.java
+++ app/src/main/java/com/example/testprogram/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.testprogram;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +15,6 @@ import com.example.testprogram.retrofit.NetworkAPIService;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -25,9 +23,9 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
-import rx.android.schedulers.AndroidSchedulers;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,40 +35,39 @@ public class MainActivity extends AppCompatActivity {
     NetworkAPIService mAPIService;
     RecyclerView list;
     TextView tv;
+    Toolbar toolbar;
     PostsAdapter adapter;
     ArrayList<Hit> hits = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+    boolean disableScroll = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        tv = toolbar.findViewById(R.id.toolbar_title);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        list =  findViewById(R.id.list_rv);
+        list = findViewById(R.id.list_rv);
+        list.setLayoutManager(new LinearLayoutManager(this));
         adapter = new PostsAdapter(R.layout.list_item, hits);
         list.setAdapter(adapter);
-        list.setLayoutManager(new LinearLayoutManager(this));
-
-//        initializeLayout(list);
         buildRetrofitService();
-        mAPIService.getPosts(1)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<retrofit2.Response<SearchItem>>() {
-                    @Override
-                    public void call(retrofit2.Response<SearchItem> response) {
-                        if (response.isSuccessful()) {
-                            searchItem = response.body();
-                            adapter.setHits(searchItem.getHits());
-                            adapter.notifyDataSetChanged();
-                            setTitle();
-                        }
-                    }
-                });
-
-
+        loadMore(1);
+        linearLayoutManager = (LinearLayoutManager) list.getLayoutManager();
+        list.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                linearLayoutManager = (LinearLayoutManager) list.getLayoutManager();
+                int totalItemCount = linearLayoutManager.getItemCount();
+                int lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+                if (!disableScroll && totalItemCount == (lastVisibleItem + 1)) {
+                    loadMore(searchItem.getPage() + 1);
+                }
+            }
+        });
     }
+
     private void buildRetrofitService() {
         Interceptor headerInterceptor = new Interceptor() {
             @Override
@@ -109,34 +106,30 @@ public class MainActivity extends AppCompatActivity {
 
         mAPIService = retrofit.create(NetworkAPIService.class);
     }
-    private void initializeLayout(RecyclerView list){
-        int visibleThreshold  = 5;
-        final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) list.getLayoutManager();
-        list.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-//                totalItemCount = linearLayoutManager.getItemCount();
-//                lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
-//                if (totalItemCount <= (lastVisibleItem + visibleThreshold)) {
-//                    if (onLoadMoreListener != null) {
-//                        onLoadMoreListener.onLoadMore();
-//                    }
-//                }
-            }
-        });
-    }
-    private void setTitle(){
-        if(searchItem !=null){
-            tv.setText(searchItem.getHits().size());
-
+    private void setTitle() {
+        if (searchItem != null) {
+            toolbar.setTitle("Total Items: " + hits.size());
         }
     }
 
-    private void setData(SearchItem data){
-        adapter = new PostsAdapter(R.layout.list_item, data.getHits());
-        list.setAdapter(adapter);
-
+    private void loadMore(int number) {
+        disableScroll = true;
+        mAPIService.getPosts(number)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<retrofit2.Response<SearchItem>>() {
+                    @Override
+                    public void call(retrofit2.Response<SearchItem> response) {
+                        disableScroll = false;
+                        if (response.isSuccessful()) {
+                            searchItem = response.body();
+                            hits.addAll(searchItem.getHits());
+                            adapter.setHits(hits);
+                            adapter.notifyDataSetChanged();
+                            setTitle();
+                        }
+                    }
+                });
     }
 
 }
